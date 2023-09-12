a9d87c3edc8b838d8798e6aacdd3ce59f31f7966
Arun Amballa <53054775+ArunAmballa@users.noreply.github.com>
2023-09-11 18:38:45 -0700

    Time: 98 ms (25.21%), Space: 17.3 MB (24.11%) - LeetHub

--- 1128-remove-all-adjacent-duplicates-in-string/1128-remove-all-adjacent-duplicates-in-string.py
+++ 1128-remove-all-adjacent-duplicates-in-string/1128-remove-all-adjacent-duplicates-in-string.py
@@ -1,18 +1,18 @@
 from queue import LifoQueue
 class Solution:
     def removeDuplicates(self, s: str) -> str:
-        st=LifoQueue()
-        ans=''
-        for i in range(len(s)):
-            if st.empty():
-                st.put(s[i])
+        st=[]
+        for i in range(0,len(s)):
+            if not st:
+                st.append(s[i])
+            elif s[i]==st[-1]:
+                st.pop()
             else:
-                if st.queue[-1]==s[i]:
-                    st.get()
-                else:
-                    st.put(s[i])
-        while not st.empty():
-            ans=ans+st.get()
-        return ans[::-1]
+                st.append(s[i])
+        s=''
+        while st:
+            s=s+st[-1]
+            st.pop()
+        return s[::-1]
 
         
