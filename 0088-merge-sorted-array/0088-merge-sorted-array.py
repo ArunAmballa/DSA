19e862aa28d8a74fe6afe8e63190bc9c0959cc76
Arun Amballa <53054775+ArunAmballa@users.noreply.github.com>
2023-09-15 11:16:17 -0700

    Time: 45 ms (56.86%), Space: 16.2 MB (81.79%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -5,8 +5,7 @@ class Solution:
         """
         p1=m-1
         p2=n-1
-        for p in range((m+n-1),-1,-1):
-            
+        for p in range(len(nums1)-1,-1,-1):
             if p1>=0 and p2>=0:
                 if nums1[p1]>nums2[p2]:
                     nums1[p]=nums1[p1]
@@ -14,10 +13,12 @@ class Solution:
                 else:
                     nums1[p]=nums2[p2]
                     p2=p2-1
-            elif p1>=0:
-                nums1[p]=nums1[p1]
-                p1=p1-1
-            else:
+            elif p1<0:
                 nums1[p]=nums2[p2]
                 p2=p2-1
+            else:
+                nums1[p]=nums1[p1]
+                p1=p1-1
             
+        
+        
