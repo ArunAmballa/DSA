3698dd953d1a70097c2fdc1fc47e871cb477822e
Arun Amballa <53054775+ArunAmballa@users.noreply.github.com>
2023-09-15 10:52:27 -0700

    Time: 76 ms (80.55%), Space: 16.7 MB (88.86%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.py
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.py
@@ -4,11 +4,11 @@ class Solution:
         suf=1
         maxi=-(1<<31)
         n=len(nums)
-        for i in range(0,len(nums)):
+        for i in range(len(nums)):
             if pre==0:pre=1
             if suf==0:suf=1
             pre=pre*nums[i]
-            suf=suf*nums[n-i-1]
+            suf=suf*nums[n-1-i]
             maxi=max(maxi,max(pre,suf))
         return maxi
         
