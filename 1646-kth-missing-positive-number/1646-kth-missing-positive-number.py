e95c2da731f5dcce8f67fa4d5e599542f3a36e39
Arun Amballa <53054775+ArunAmballa@users.noreply.github.com>
2023-09-20 19:24:06 -0700

    Time: 55 ms (72.97%), Space: 16.5 MB (56.33%) - LeetHub

--- 1646-kth-missing-positive-number/1646-kth-missing-positive-number.py
+++ 1646-kth-missing-positive-number/1646-kth-missing-positive-number.py
@@ -4,11 +4,10 @@ class Solution:
         hi=len(arr)-1
         while lo<=hi:
             mid=lo+(hi-lo)//2
-            missing=arr[mid]-(mid+1)
-            if missing<k:
+            missings=arr[mid]-(mid+1)
+            if missings<k:
                 lo=mid+1
             else:
                 hi=mid-1
         return lo+k
-
         
