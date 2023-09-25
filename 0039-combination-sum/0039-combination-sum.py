fc8c1fd1d118d757dd39e95f66c7a5142d7aef25
Arun Amballa <53054775+ArunAmballa@users.noreply.github.com>
2023-09-25 11:56:44 -0700

    Time: 72 ms (23.29%), Space: 16.1 MB (99.38%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -1,17 +1,20 @@
 class Solution:
-    def generate(self,candidates,ind,target,l,ans):
+    def generate(self,candidates,target,ind,l,ans):
         if target==0:
             ans.append(l.copy())
             return 
-        if target<0:
+        if target<0 or ind==len(candidates):
             return 
-        for i in range(ind,len(candidates)):
-            l=l+[candidates[i]]
-            self.generate(candidates,i,target-candidates[i],l,ans)
-            l.pop()
+        #Take Case 
+        l=l+[candidates[ind]]
+        self.generate(candidates,target-candidates[ind],ind,l,ans)
+        l.pop()
+        #Not Take Case
+        self.generate(candidates,target,ind+1,l,ans)
+        # final return ans
         return ans
 
-        
+
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        return self.generate(candidates,0,target,[],[])
+        return self.generate(candidates,target,0,[],[])
         
