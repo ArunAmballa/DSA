fc7f61167637c95db9a82811e15b0753d8db5f8d
Arun Amballa <53054775+ArunAmballa@users.noreply.github.com>
2023-09-25 11:20:35 -0700

    Time: 35 ms (88.92%), Space: 16.4 MB (91.99%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,15 +1,17 @@
 class Solution:
-    def check(self,nums,i,l,ans):
-        if i==len(nums):
+    def generate(self,nums,ind,l,ans):
+        if ind==len(nums):
             ans.append(l.copy())
             return 
-        #Take Case
-        l=l+[nums[i]]
-        self.check(nums,i+1,l,ans)
+        #Take Case:
+        l=l+[nums[ind]]
+        self.generate(nums,ind+1,l,ans)
         l.pop()
-        # Dont Take Case
-        self.check(nums,i+1,l,ans)
+        #Not Take
+        self.generate(nums,ind+1,l,ans)
+        #Return Final Answer
         return ans
+
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        return self.check(nums,0,[],[])
+        return self.generate(nums,0,[],[])
         
