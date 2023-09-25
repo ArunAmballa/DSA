9642892bc1ca525bc21425077ac0700cc2220263
Arun Amballa <53054775+ArunAmballa@users.noreply.github.com>
2023-09-25 13:06:25 -0700

    Time: 37 ms (67.8%), Space: 16.1 MB (99.07%) - LeetHub

--- 0050-powx-n/0050-powx-n.py
+++ 0050-powx-n/0050-powx-n.py
@@ -1,10 +1,10 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-        if n<0:
-            n=-1*n
-            x=1.0/x
         if n==0:
             return 1
+        if n<0:
+            n=-(n)
+            x=1/x
         ans=1
         for i in range(0,int(math.log(n,2))+1):
             if n&(1<<i)!=0:
