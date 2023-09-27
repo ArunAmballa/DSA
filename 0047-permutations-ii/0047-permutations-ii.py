955b10f9f5a6a3bb4719a2243cbec50f7e667309
Arun Amballa <53054775+ArunAmballa@users.noreply.github.com>
2023-09-27 12:44:44 -0700

    Time: 56 ms (77.2%), Space: 16.3 MB (97.82%) - LeetHub

---
+++ 0047-permutations-ii/0047-permutations-ii.py
@@ -0,0 +1,16 @@
+class Solution:
+    def generate(self,nums,ans,ind):
+        if ind==len(nums):
+            ans.append(nums.copy())
+            return 
+        s=set()
+        for i in range(ind,len(nums)):
+            if (nums[i]) in s:continue
+            s.add(nums[i])
+            nums[i],nums[ind]=nums[ind],nums[i]
+            self.generate(nums,ans,ind+1)
+            nums[i],nums[ind]=nums[ind],nums[i]
+        return ans
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        return self.generate(nums,[],0)
