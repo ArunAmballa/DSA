77ec9b090a1959960439417b1b564e1c5989ce00
Arun Amballa <53054775+ArunAmballa@users.noreply.github.com>
2023-10-02 12:11:09 -0700

    Time: 182 ms (56.41%), Space: 22.6 MB (33.35%) - LeetHub

--- 0816-design-hashset/0816-design-hashset.py
+++ 0816-design-hashset/0816-design-hashset.py
@@ -1,20 +1,44 @@
+class Node:
+    def __init__(self,key):
+        self.key=key 
+        self.next=None
 class MyHashSet:
 
     def __init__(self):
-        self.l=[-1]*1000001
+        self.set=[Node(-1) for i in range(1000)]
         
+    def hash(self,key):
+        return key%1000
 
     def add(self, key: int) -> None:
-        self.l[key]=1
-        
+        curr=self.set[self.hash(key)]
+        temp=curr
+        while curr!=None:
+            if curr.key==key:
+                return
+            curr=curr.next
+        newNode=Node(key)
+        newNode.next=temp.next
+        temp.next=newNode
 
     def remove(self, key: int) -> None:
-        self.l[key]=-1
+        curr=self.set[self.hash(key)]
+        while curr!=None:
+            if curr.next==None:
+                return
+            if curr.next.key==key:
+                curr.next=curr.next.next
+            curr=curr.next
+
         
 
     def contains(self, key: int) -> bool:
-        return self.l[key]!=-1
-        
+        curr=self.set[self.hash(key)]
+        while curr!=None:
+            if curr.key==key:
+                return True
+            curr=curr.next
+        return False
 
 
 # Your MyHashSet object will be instantiated and called as such:
