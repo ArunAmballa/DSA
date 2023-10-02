8e7144aff3a616c38d3aa6d095bc4e3a1b1c5d19
Arun Amballa <53054775+ArunAmballa@users.noreply.github.com>
2023-10-02 11:33:46 -0700

    Time: 324 ms (22.58%), Space: 22.4 MB (20.31%) - LeetHub

--- 0817-design-hashmap/0817-design-hashmap.py
+++ 0817-design-hashmap/0817-design-hashmap.py
@@ -1,19 +1,41 @@
+class Node:
+    def __init__(self,key,val):
+        self.key=key
+        self.val=val
+        self.next=None
 class MyHashMap:
 
     def __init__(self):
-        self.map=[-1]*1000001
+        self.d=[Node(-1,-1) for i in range(0,10000)]
+    
+    def hash(self,key):
+        return key%len(self.d)
         
-
     def put(self, key: int, value: int) -> None:
-        self.map[key]=value
+        curr=self.d[self.hash(key)]
+        while curr.next!=None:
+            if curr.next.key==key:
+                curr.next.val=value
+                return
+            curr=curr.next
+        curr.next=Node(key,value)
         
-
     def get(self, key: int) -> int:
-        return self.map[key]
-        
+        curr=self.d[self.hash(key)]
+        while curr!=None:
+            if curr.key==key:
+                return curr.val
+            curr=curr.next
+        return -1
 
     def remove(self, key: int) -> None:
-        self.map[key]=-1
+        curr=self.d[self.hash(key)]
+        while curr.next:
+            if curr.next.key==key:
+                curr.next=curr.next.next
+                return 
+            curr=curr.next
+        
 
 
 # Your MyHashMap object will be instantiated and called as such:
