414d0f594341404147a85a6ddd5602bc245d8cac
Arun Amballa <53054775+ArunAmballa@users.noreply.github.com>
2023-10-06 14:26:02 -0700

    Time: 101 ms (48.26%), Space: 21 MB (60.25%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,18 +1,29 @@
 class Solution:
+    def partition(self,nums,lo,hi,d):
+        pivot=nums[lo]
+        i=lo
+        j=hi
+        while i<j:
+            while d[nums[i]]<=d[pivot] and i<hi:
+                i=i+1
+            while d[nums[j]]>d[pivot] and j>lo:
+                j=j-1
+            if i<j:
+                nums[i],nums[j]=nums[j],nums[i]
+        nums[lo],nums[j]=nums[j],nums[lo]
+        return j
+    def QuickSelect(self,nums,lo,hi,k,d):
+        pIndex=self.partition(nums,lo,hi,d)
+        if pIndex==k:
+            return nums[k:]
+        elif k<pIndex:
+            return self.QuickSelect(nums,lo,pIndex-1,k,d)
+        else:
+            return self.QuickSelect(nums,pIndex+1,hi,k,d)
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         d={}
         for i in range(len(nums)):
             d[nums[i]]=d.get(nums[i],0)+1
-        h=[]
-        for i in d:
-            if len(h)<k:
-                heapq.heappush(h,(d[i],i))
-            elif len(h)==k:
-                if d[i]>h[0][0]:
-                    heapq.heapreplace(h,(d[i],i))
-        ans=[]
-        while h:
-            frequency,element=heapq.heappop(h)
-            ans.append(element)
-        return ans
+        nums=list(d.keys())
+        return self.QuickSelect(nums,0,len(nums)-1,len(nums)-k,d)
         
