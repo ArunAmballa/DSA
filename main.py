d9c17fcc993ef6e38824b5f8f54cc23235bb8d52
Arun Amballa <53054775+ArunAmballa@users.noreply.github.com>
2023-10-11 12:19:40 -0700

    Product of three Numbers- Code Forces

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
-def check(rotations):
-    n=len(rotations)
-    for i in range(0,(1<<n)):
-        ans=0
-        for j in range(0,n):
-            if i&(1<<j)!=0:
-                ans=ans+rotations[j]
-            else:
-                ans=ans-rotations[j]
-        if ans==0:
-            print("Yes")
-    print("NO")
-rotations=int(input())
-l=[]
-for i in range(rotations):
+import math
+def check(n):
+    a=1 
+    b=1 
+    c=1 
+    for i in range(2,int(math.sqrt(n))+1):
+        if n%i==0:
+            a=i 
+            break
+    n=n//a 
+    for i in range(2,int(math.sqrt(n))+1):
+        if n%i==0 and i!=a:
+            b=i 
+            break
+    c=n//b 
+    if (a!=b and b!=c and a!=1 and b!=1 and c!=1):
+        print("Yes")
+        print(a,b,c,sep=" ")
+    else:
+        print("No")
+t=int(input())
+for i in range(t):
     n=int(input())
-    l.append(n)
-check(l)
+    check(n)
