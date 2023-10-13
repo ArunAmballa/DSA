7f2f611777c576954bb4c369fc3a13bdec42c9d8
Arun Amballa <53054775+ArunAmballa@users.noreply.github.com>
2023-10-13 14:14:27 -0700

    Updated Solution - GfG to GitHub

--- Medium/Maximum XOR of two numbers in an array/maximum-xor-of-two-numbers-in-an-array.py
+++ Medium/Maximum XOR of two numbers in an array/maximum-xor-of-two-numbers-in-an-array.py
@@ -1,39 +1,38 @@
 #User function Template for python3
 class TrieNode:
     def __init__(self):
-        self.children=[None]*2
+        self.children={}
 class Trie:
     def __init__(self):
         self.root=TrieNode()
-    def insert(self,n,maxLen):
+    def insert(self,number,n):
         curr=self.root
-        for i in range(maxLen,-1,-1):
-            index=1 if n&(1<<i)!=0 else 0
-            if curr.children[index]==None:
-                curr.children[index]=TrieNode()
-            curr=curr.children[index]
-    def getMax(self,number,maxLen):
-        curr=self.root
-        maxNumber=0
-        for i in range(maxLen,-1,-1):
+        for i in range(n,-1,-1):
             bit=1 if number&(1<<i)!=0 else 0
-            if curr.children[1-bit]!=None:
-                maxNumber=maxNumber|(1<<i)
+            if bit not in curr.children:
+                curr.children[bit]=TrieNode()
+            curr=curr.children[bit]
+    def search(self,number,n):
+        curr=self.root
+        ans=0
+        for i in range(n,-1,-1):
+            bit=1 if number&(1<<i)!=0 else 0
+            if 1-bit in curr.children:
+                ans=ans|(1<<i)
                 curr=curr.children[1-bit]
             else:
                 curr=curr.children[bit]
-        return maxNumber
+        return ans
 class Solution:
     def max_xor(self, arr, n):
-        maxLen=len(bin(max(arr)))-2
         obj=Trie()
+        n=len(bin(max(arr)))-2
         ans=0
-        for i in range(len(arr)):
-            obj.insert(arr[i],maxLen)
+        for i in range(0,len(arr)):
+            obj.insert(arr[i],n)
             if i>0:
-                ans=max(ans,obj.getMax(arr[i],maxLen))
+                ans=max(ans,obj.search(arr[i],n))
         return ans
-       
 
 
 #{ 
