5ed22f17dcead6e8259f2cc341e53a58bf1d0a84
Arun Amballa <53054775+ArunAmballa@users.noreply.github.com>
2023-11-07 09:52:32 -0800

    Time: 371 ms (39.38%), Space: 71 MB (52.85%) - LeetHub

--- 1475-maximum-sum-bst-in-binary-tree/1475-maximum-sum-bst-in-binary-tree.py
+++ 1475-maximum-sum-bst-in-binary-tree/1475-maximum-sum-bst-in-binary-tree.py
@@ -5,23 +5,21 @@
 #         self.left = left
 #         self.right = right
 class Solution:
+    def helper(self,root):
+        if root==None:
+            #isBST,Size,MaxValue,MinValue
+            return [True,0,-math.inf,math.inf]
+        leftCheck=self.helper(root.left)
+        rightCheck=self.helper(root.right)
+        if ((leftCheck[0]==True) and (rightCheck[0]==True) and (root.val>leftCheck[2]) and (root.val<rightCheck[3])):
+            currSize=leftCheck[1]+rightCheck[1]+root.val
+            self.maxSum=max(self.maxSum,currSize)
+            return [True,currSize,max(root.val,rightCheck[2]),min(root.val,leftCheck[3])]
+        return [False,max(leftCheck[1],rightCheck[1]),-math.inf,math.inf]
     def maxSumBST(self, root: Optional[TreeNode]) -> int:
         if root==None:
             return 0
-        def helper(root):
-            nonlocal maxSum
-            if root==None:
-                return [True,0,-math.inf,math.inf]
-            leftAns=helper(root.left)
-            rightAns=helper(root.right)
-            if leftAns[0]==True and rightAns[0]==True and root.val>leftAns[2] and root.val<rightAns[3]:
-                currSum=leftAns[1]+rightAns[1]+root.val
-                maxSum=max(maxSum,currSum)
-                return [True,currSum,max(rightAns[2],root.val),min(leftAns[3],root.val)]
-            else:
-                return [False,max(leftAns[1],rightAns[1]),-math.inf,math.inf]
-
-        maxSum=0
-        helper(root)
-        return maxSum
+        self.maxSum=0
+        self.helper(root)
+        return self.maxSum
         
