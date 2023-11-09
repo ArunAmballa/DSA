aed64b24fbf780e1dd3ac481c5711154622eb054
Arun Amballa <53054775+ArunAmballa@users.noreply.github.com>
2023-11-09 09:58:44 -0800

    Updated Solution - GfG to GitHub

--- Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.py
+++ Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.py
@@ -14,8 +14,17 @@ class Solution:
             dp[n][W]=self.helper(wt,val,W,n-1,dp)
         return dp[n][W]
     def knapSack(self,W, wt, val, n):
-        dp=[[-1 for j in range(W+1)]for i in range(n+1)]
-        return self.helper(wt,val,W,n,dp)
+        dp=[[0 for j in range(W+1)]for i in range(n+1)]
+        for i in range(0,n+1):
+            for j in range(0,W+1):
+                if i==0 or j==0:
+                    dp[i][j]=0
+                    continue
+                if wt[i-1]<=j:
+                    dp[i][j]=max((val[i-1]+dp[i-1][j-wt[i-1]]),dp[i-1][j])
+                else:
+                    dp[i][j]=dp[i-1][j]
+        return dp[n][W]
        
 
 
