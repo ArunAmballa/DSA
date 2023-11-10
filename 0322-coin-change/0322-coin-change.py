42e184658a87606dacb3f8c51dbe2e361e364a43
Arun Amballa <53054775+ArunAmballa@users.noreply.github.com>
2023-11-10 11:21:52 -0800

    Time: 1616 ms (8.16%), Space: 17.8 MB (28.47%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,21 +1,41 @@
 class Solution:
-    def unboundedk(self,wt,w,n,dp):
+    def helper(self,n,w,dp,wt):
         if w==0:
             return 0
         if n==0:
-            return 2<<31
+            return 1<<31
         if dp[n][w]!=-1:
             return dp[n][w]
         if wt[n-1]<=w:
-            dp[n][w]=min((1+self.unboundedk(wt,w-wt[n-1],n,dp)),self.unboundedk(wt,w,n-1,dp))
+            dp[n][w]=min((1+self.helper(n,w-wt[n-1],dp,wt)),self.helper(n-1,w,dp,wt))
         else:
-            dp[n][w]=self.unboundedk(wt,w,n-1,dp)
-        return dp[n][w] 
+            dp[n][w]=self.helper(n-1,w,dp,wt)
+        return dp[n][w]
+
     def coinChange(self, coins: List[int], amount: int) -> int:
         dp=[[-1 for j in range(amount+1)]for i in range(len(coins)+1)]
-        ans=self.unboundedk(coins,amount,len(coins),dp)
-        if ans>amount:
-            return -1
-        return ans
+        # ans=self.helper(len(coins),amount,dp,coins)
+        # if ans>amount:
+        #     return -1
+        # return ans
         
-    
+        n=len(coins)
+        w=amount
+        wt=coins
+        # prev=[0]*(w+1)
+        curr=[0]*(w+1)
+        for i in range(0,len(coins)+1):
+            for j in range(0,amount+1):
+                if j==0:
+                    curr[j]=0
+                    continue
+                if i==0:
+                    curr[j]=1<<31
+                    continue
+                if wt[i-1]<=j:
+                    curr[j]=min(1+curr[j-wt[i-1]],prev[j])
+                else:
+                    curr[j]=prev[j]
+            prev=curr[:]
+        return -1 if curr[w]>amount else curr[w]
+        
