dc54eed332d1515d4a685709a850b5087e74754a
Arun Amballa <53054775+ArunAmballa@users.noreply.github.com>
2023-11-14 14:59:32 -0800

    Time: 41 ms (41.99%) | Memory: 16.2 MB (49.89%) - LeetSync

--- 62-unique-paths/unique-paths.py
+++ 62-unique-paths/unique-paths.py
@@ -9,18 +9,21 @@ class Solution:
         dp[m][n]=self.helper(m-1,n,dp)+self.helper(m,n-1,dp)
         return dp[m][n]
     def uniquePaths(self, m: int, n: int) -> int:
-        dp=[[0 for j in range(n)]for i in range(m)]
+        
         # return self.helper(m-1,n-1,dp)
+        prev=[0]*(n)
         for i in range(m):
+            curr=[0]*(n)
             for j in range(n):
                 if i<0 or j<0:
-                    dp[i][j]=0
+                    curr[j]=0
                     continue
                 if i==0 and j==0:
-                    dp[i][j]=1
+                    curr[j]=1
                     continue
-                dp[i][j]=dp[i-1][j]+dp[i][j-1]
-        return dp[m-1][n-1]
+                curr[j]=prev[j]+curr[j-1]
+            prev=curr
+        return curr[n-1]
 
 
         
