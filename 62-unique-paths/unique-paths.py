8570f635aeb8860ab146c728eb4a45842a950c6d
Arun Amballa <53054775+ArunAmballa@users.noreply.github.com>
2023-11-14 14:52:30 -0800

    Time: 36 ms (75.21%) | Memory: 16.5 MB (7.11%) - LeetSync

--- 62-unique-paths/unique-paths.py
+++ 62-unique-paths/unique-paths.py
@@ -1,29 +1,29 @@
 class Solution:
-    # def helper(self,m,n,dp):
-    #     if m<0 or n<0:
-    #         return 0
-    #     if m==0 or n==0:
-    #         return 1
-    #     if dp[m][n]!=-1:
-    #         return dp[m][n]
-    #     dp[m][n]=self.helper(m-1,n,dp)+self.helper(m,n-1,dp)
-    #     return dp[m][n]
+    def helper(self,m,n,dp):
+        if m<0 or n<0:
+            return 0
+        if m==0 and n==0:
+            return 1
+        if dp[m][n]!=-1:
+            return dp[m][n]
+        dp[m][n]=self.helper(m-1,n,dp)+self.helper(m,n-1,dp)
+        return dp[m][n]
     def uniquePaths(self, m: int, n: int) -> int:
-        # dp=[[-1 for j in range(n)]for i in range(m)]
-        # return self.helper(m-1,n-1,dp)
-        prev=[-1]*(n)
-        for i in range(m):
-            curr=[-1]*(n)
-            for j in range(n):
-                if i<0 or j<0:
-                    curr[j]=0
-                    continue
-                if i==0 or j==0:
-                    curr[j]=1
-                    continue
-                curr[j]=prev[j]+curr[j-1]
-            prev=curr
-        return curr[n-1]
+        dp=[[-1 for j in range(n)]for i in range(m)]
+        return self.helper(m-1,n-1,dp)
+        # prev=[-1]*(n)
+        # for i in range(m):
+        #     curr=[-1]*(n)
+        #     for j in range(n):
+        #         if i<0 or j<0:
+        #             curr[j]=0
+        #             continue
+        #         if i==0 or j==0:
+        #             curr[j]=1
+        #             continue
+        #         curr[j]=prev[j]+curr[j-1]
+        #     prev=curr
+        # return curr[n-1]
 
 
         
