bc5658c91a01a7e32df6ce7a3350e3fe5d57d8b0
Arun Amballa <53054775+ArunAmballa@users.noreply.github.com>
2023-11-14 15:18:57 -0800

    Time: 104 ms (24.93%) | Memory: 18.1 MB (37.32%) - LeetSync

---
+++ 64-minimum-path-sum/minimum-path-sum.py
@@ -0,0 +1,16 @@
+class Solution:
+    def helper(self,n,m,grid,dp):
+        if n<0 or m<0:
+            return 1<<31
+        if n==0 and m==0:
+            return grid[n][m]
+        if dp[n][m]!=-1:
+            return dp[n][m]
+        dp[n][m]=grid[n][m]+min(self.helper(n-1,m,grid,dp),self.helper(n,m-1,grid,dp))
+        return dp[n][m]
+    def minPathSum(self, grid: List[List[int]]) -> int:
+        n=len(grid)
+        m=len(grid[0])
+        dp=[[-1 for j in range(m)]for i in range(n)]
+        return self.helper(n-1,m-1,grid,dp)
+        
