7e3e0c3b8382ca8cf4f44e007c3c57936e53da75
Arun Amballa <53054775+ArunAmballa@users.noreply.github.com>
2023-11-14 14:55:45 -0800

    Time: 49 ms (6.20%) | Memory: 16.4 MB (22.46%) - LeetSync

--- 62-unique-paths/unique-paths.py
+++ 62-unique-paths/unique-paths.py
@@ -1,9 +1,9 @@
 class Solution:
     def helper(self,m,n,dp):
-        if m<0 or n<0:
-            return 0
         if m==0 and n==0:
             return 1
+        if m<0 or n<0:
+            return 0
         if dp[m][n]!=-1:
             return dp[m][n]
         dp[m][n]=self.helper(m-1,n,dp)+self.helper(m,n-1,dp)
@@ -15,11 +15,11 @@ class Solution:
         # for i in range(m):
         #     curr=[-1]*(n)
         #     for j in range(n):
+        #         if i==0 and j==0:
+        #             curr[j]=1
+        #             continue
         #         if i<0 or j<0:
         #             curr[j]=0
-        #             continue
-        #         if i==0 or j==0:
-        #             curr[j]=1
         #             continue
         #         curr[j]=prev[j]+curr[j-1]
         #     prev=curr
