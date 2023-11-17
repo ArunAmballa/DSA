62e695f0ead8e32d9f084165cc40cf2e16800b73
Arun Amballa <53054775+ArunAmballa@users.noreply.github.com>
2023-11-16 19:36:35 -0800

    Time: 131 ms (69.05%) | Memory: 17.1 MB (54.73%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.py
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.py
@@ -14,17 +14,20 @@ class Solution:
     def minFallingPathSum(self, matrix: List[List[int]]) -> int:
         ans=1<<31
         n=len(matrix)
-        dp=[[0 for j in range(n)]for i in range(n)]
+        # dp=[[0 for j in range(n)]for i in range(n)]
+        prev=[0]*(n)
         for j in range(n):
-            dp[0][j]=matrix[0][j]
+            prev[j]=matrix[0][j]
         for i in range(1,n):
+            curr=[0]*n
             for j in range(0,n):
-                left=matrix[i][j]+(dp[i-1][j-1] if j-1>=0 else 1<<31)
-                down=matrix[i][j]+(dp[i-1][j])
-                right=matrix[i][j]+(dp[i-1][j+1] if j+1<n else 1<<31)
-                dp[i][j]=min(left,min(down,right))
+                left=matrix[i][j]+(prev[j-1] if j-1>=0 else 1<<31)
+                down=matrix[i][j]+(prev[j])
+                right=matrix[i][j]+(prev[j+1] if j+1<n else 1<<31)
+                curr[j]=min(left,min(down,right))
+            prev=curr
         for j in range(0,n):
-            ans=min(ans,dp[n-1][j])
+            ans=min(ans,prev[j])
         return ans
             
         
