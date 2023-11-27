4ecad2a03e130fce64875f5786cdc1cfc7df9006
Arun Amballa <53054775+ArunAmballa@users.noreply.github.com>
2023-11-27 10:14:30 -0800

    Updated Solution - GfG to GitHub

--- Hard/Matrix Chain Multiplication/matrix-chain-multiplication.py
+++ Hard/Matrix Chain Multiplication/matrix-chain-multiplication.py
@@ -1,31 +1,20 @@
 #User function Template for python3
 
 class Solution:
-    def helper(self,i,j,arr):
-        if i==j:
+    def helper(self,i,j,arr,dp):
+        if i>=j:
             return 0
+        if dp[i][j]!=-1:
+            return dp[i][j]
         ans=1<<31
         for k in range(i,j):
-            tempans=self.helper(i,k,arr)+self.helper(k+1,j,arr)+(arr[i-1]*arr[j]*arr[k])
-            ans=min(tempans,ans)
-        return ans
+            tempAns=self.helper(i,k,arr,dp)+self.helper(k+1,j,arr,dp)+(arr[i-1]*arr[j]*arr[k])
+            ans=min(tempAns,ans)
+        dp[i][j]=ans
+        return dp[i][j]
     def matrixMultiplication(self, N, arr):
-        dp=[[-1 for j in range(len(arr))]for i in range(len(arr))]
-        # for i in range(0,N):
-        #     dp[i][i]=0
-        for i in range(N-1,0,-1):
-            for j in range(0,N):
-                if i>=j:
-                    dp[i][j]=0
-                    continue
-                ans=1<<31
-                for k in range(i,j):
-                    tempAns=dp[i][k]+dp[k+1][j]+(arr[i-1]*arr[k]*arr[j])
-                    ans=min(ans,tempAns)
-                dp[i][j]=ans
-        return dp[1][N-1] 
-
-        
+        dp=[[-1 for j in range(N)]for i in range(N)]
+        return self.helper(1,N-1,arr,dp)
 
 
 #{ 
