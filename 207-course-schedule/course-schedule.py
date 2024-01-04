af66d64663eeefc1c2351af9ef79f520abce8dcc
Arun Amballa <53054775+ArunAmballa@users.noreply.github.com>
2024-01-04 11:32:30 -0800

    Time: 99 ms (56.87%) | Memory: 19.1 MB (64.76%) - LeetSync

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -1,39 +1,28 @@
 from queue import Queue
 class Solution:
-    def helper(self,graph,numCourses):
-        indegree={i:0 for i in range(numCourses)}
-        for i in graph:
-            for el in graph[i]:
-                indegree[el]=indegree[el]+1
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        adj={i:[] for i in range(numCourses)}
+        for edge in prerequisites:
+            u,v=edge
+            adj[v].append(u)
+        indegree=[0]*numCourses
+        for i in range(numCourses):
+            for j in adj[i]:
+                indegree[j]=indegree[j]+1
         q=Queue()
-        visited=[0]*numCourses
         for i in range(numCourses):
             if indegree[i]==0:
                 q.put(i)
-                visited[i]=1
-        cnt=0
+        ans=[]
         while not q.empty():
             curr=q.get()
-            cnt=cnt+1
-            for ne in graph[curr]:
+            ans.append(curr)
+            for ne in adj[curr]:
                 indegree[ne]=indegree[ne]-1
-                if visited[ne]==0 and indegree[ne]==0:
+                if indegree[ne]==0:
                     q.put(ne)
-                    visited[ne]=1
-        if cnt==numCourses:
+        if len(ans)==numCourses:
             return True
         return False
 
 
-            
-
-
-
-    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        graph={i:[] for i in range(numCourses)}
-        for edge in prerequisites:
-            u,v=edge
-            graph[v].append(u)
-        return self.helper(graph,numCourses)
-        
-        
