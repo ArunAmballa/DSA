405a12fbe2545ed110d8583f9fd9f471b0f2507a
Arun Amballa <53054775+ArunAmballa@users.noreply.github.com>
2024-01-29 15:01:49 -0800

    Time: 50 ms (43.99%) | Memory: 17.5 MB (77.14%) - LeetSync

--- 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.py
+++ 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.py
@@ -4,33 +4,33 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def reverse(self,head,k,groups):
-        if head==None or head.next==None:
-            return head
-        prev=None
+    def reverse(self,head,k,group):
         temp=head
+        prev=None
         cnt=0
+
         while temp!=None and cnt<k:
             forward=temp.next
             temp.next=prev
             prev=temp
             temp=forward
             cnt=cnt+1
-        if forward!=None and groups!=0:
-            head.next=self.reverse(forward,k,groups-1)
+        if forward!=None and group!=0:
+            head.next=self.reverse(forward,k,group-1)
         else:
             head.next=forward
         return prev
-    def size(self,head):
+    def length(self,head):
         cnt=0
         temp=head
         while temp!=None:
-            cnt+=1
+            cnt=cnt+1
             temp=temp.next
         return cnt
     def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
         if head==None or head.next==None:
             return head
-        length=self.size(head)
-        number=length//k
-        return self.reverse(head,k,number-1)
+        size=self.length(head)
+        groups=size//k
+        return self.reverse(head,k,groups-1)
+        
