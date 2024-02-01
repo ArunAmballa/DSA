365f1429912351cb8f9c9e55ce96faffd1b362a9
Arun Amballa <53054775+ArunAmballa@users.noreply.github.com>
2024-02-01 13:24:23 -0800

    Time: 241 ms (37.59%) | Memory: 17.2 MB (99.89%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.py
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.py
@@ -1,12 +1,25 @@
 class Solution:
-    def numSubarraysWithSum(self, nums: List[int], goal: int) -> int:
-        d={0:1}
-        preSum=0
+    def helper(self,nums,goal):
+        s=0
+        e=0
         ans=0
-        for i in range(len(nums)):
-            preSum=preSum+nums[i]
-            target=preSum-goal
-            if target in d:
-                ans=ans+d[target]
-            d[preSum]=d.get(preSum,0)+1
+        preSum=0
+        while e<len(nums):
+            preSum=preSum+nums[e]
+            if preSum<=goal:
+                ans=ans+ (e-s+1)
+                e=e+1
+            else:
+                while preSum > goal and s<=e:
+                    preSum=preSum-nums[s]
+                    s=s+1
+                ans+=e-s+1
+                e=e+1
         return ans
+
+    def numSubarraysWithSum(self, nums: List[int], goal: int) -> int:
+        ans1=self.helper(nums,goal)
+        ans2=self.helper(nums,goal-1)
+        return ans1-ans2
+
+        
