c5808309f260f6a86e8b5a066d04ddb0ff1fc438
Arun Amballa <53054775+ArunAmballa@users.noreply.github.com>
2024-02-01 12:14:34 -0800

    Time: 692 ms (58.86%) | Memory: 24.2 MB (60.18%) - LeetSync

---
+++ 940-fruit-into-baskets/fruit-into-baskets.py
@@ -0,0 +1,24 @@
+class Solution:
+    def totalFruit(self, fruits: List[int]) -> int:
+        if len(fruits)==0 or len(fruits)==1:
+            return len(fruits)
+        d={}
+        s=0
+        e=0
+        ans=-(1<<31)
+        while e<len(fruits):
+            d[fruits[e]]=d.get(fruits[e],0)+1
+            if len(d) <= 2:
+                ans=max(ans,e-s+1)
+                e=e+1
+            else:
+                while len(d)>2:
+                    d[fruits[s]]=d.get(fruits[s],0)-1
+                    if d[fruits[s]]==0:
+                        del d[fruits[s]]
+                    s=s+1
+                if len(d)==2:
+                    ans=max(ans,e-s+1)
+                e=e+1
+        return ans
+        
