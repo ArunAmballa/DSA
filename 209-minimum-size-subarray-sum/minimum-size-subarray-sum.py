1cee20089a69e2b3a6f0cca2df43160f31271f0d
Arun Amballa <53054775+ArunAmballa@users.noreply.github.com>
2024-01-31 16:41:26 -0800

    Time: 205 ms (54.54%) | Memory: 30.4 MB (53.22%) - LeetSync

--- 209-minimum-size-subarray-sum/minimum-size-subarray-sum.py
+++ 209-minimum-size-subarray-sum/minimum-size-subarray-sum.py
@@ -6,11 +6,14 @@ class Solution:
         s=0
         while end<len(nums):
             s=s+nums[end]
-            while s>=target:
-                ans=min(ans,end-start+1)
-                s=s-nums[start]
-                start=start+1
-            end=end+1
+            if s<target:
+                end=end+1
+            else:
+                while s>=target:
+                    ans=min(ans,end-start+1)
+                    s=s-nums[start]
+                    start=start+1
+                end=end+1
         if ans==1<<31:
             return 0
         return ans
