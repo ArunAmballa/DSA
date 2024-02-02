412a15c70c2c1a7ec38cb650db8c0d567874d435
Arun Amballa <53054775+ArunAmballa@users.noreply.github.com>
2024-02-01 16:40:20 -0800

    Time: 282 ms (39.45%) | Memory: 17.2 MB (91.51%) - LeetSync

--- 159-longest-substring-with-at-most-two-distinct-characters/longest-substring-with-at-most-two-distinct-characters.py
+++ 159-longest-substring-with-at-most-two-distinct-characters/longest-substring-with-at-most-two-distinct-characters.py
@@ -10,7 +10,7 @@ class Solution:
                 ans=max(ans,e-st+1)
                 e=e+1
             else:
-                while len(d)>2:
+                while len(d)>2 and st<=e:
                     d[s[st]]=d.get(s[st],0)-1
                     if d[s[st]]==0:
                         del d[s[st]]
