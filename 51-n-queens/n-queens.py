368a79abde127e7b5ba236d6c12cb45d42f78a52
Arun Amballa <53054775+ArunAmballa@users.noreply.github.com>
2024-03-17 16:40:46 -0700

    Time: 48 ms (78.48%) | Memory: 17 MB (44.67%) - LeetSync

--- 51-n-queens/n-queens.py
+++ 51-n-queens/n-queens.py
@@ -25,19 +25,28 @@ class Solution:
         
         return True
 
-    def helper(self,n,col,board,ans):
+    def helper(self,n,col,board,ans,leftRow,lowerDiagonal,upperDiagonal):
         if col==n:
             ans.append(board.copy())
             return 
         for row in range(0,n):
-            if(self.isSafe(board,col,row,n)):
+            if((row not in leftRow) and (row+col not in lowerDiagonal) and (n-1+col-row not in upperDiagonal)):
                 board[row]=board[row][:col]+"Q"+board[row][col+1:]
-                self.helper(n,col+1,board,ans)
+                leftRow.add(row)
+                lowerDiagonal.add(row+col) 
+                upperDiagonal.add(n-1+col-row)
+                self.helper(n,col+1,board,ans,leftRow,lowerDiagonal,upperDiagonal)
+                leftRow.remove(row)
+                lowerDiagonal.remove(row+col) 
+                upperDiagonal.remove(n-1+col-row)
                 board[row]=board[row][:col]+"."+board[row][col+1:]
                  
     def solveNQueens(self, n: int) -> List[List[str]]:
         ans=[]
+        leftRow=set()
+        upperDiagonal=set()
+        lowerDiagonal=set()
         board=["."*n for i in range(n)]
-        self.helper(n,0,board,ans)
+        self.helper(n,0,board,ans,leftRow,lowerDiagonal,upperDiagonal)
         return ans
         
