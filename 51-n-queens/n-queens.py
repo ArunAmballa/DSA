e475a754e9c886ec773d2d196d0f08c928f7f7f6
Arun Amballa <53054775+ArunAmballa@users.noreply.github.com>
2024-03-17 16:12:09 -0700

    Time: 76 ms (30.10%) | Memory: 16.8 MB (97.55%) - LeetSync

---
+++ 51-n-queens/n-queens.py
@@ -0,0 +1,45 @@
+class Solution:
+    def isSafe(self,board,col,row,n):
+        originalRow=row
+        originalCol=col
+
+        while (col>=0):
+            if board[row][col]=="Q":
+                return False
+            col=col-1
+        row=originalRow
+        col=originalCol
+        while (row>=0 and col>=0):
+            if board[row][col]=="Q":
+                return False
+            row=row-1
+            col=col-1
+        
+        row=originalRow
+        col=originalCol
+        while(row<n and col>=0):
+            if board[row][col]=="Q":
+                return False
+            row=row+1
+            col=col-1
+        
+        return True
+
+    def helper(self,n,col,board,ans):
+        if col==n:
+            ans.append(board.copy())
+            return 
+        for row in range(0,n):
+            if(self.isSafe(board,col,row,n)):
+                board[row]=board[row][:col]+"Q"+board[row][col+1:]
+                self.helper(n,col+1,board,ans)
+                board[row]=board[row][:col]+"."+board[row][col+1:]
+            
+
+        
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        ans=[]
+        board=["."*n for i in range(n)]
+        self.helper(n,0,board,ans)
+        return ans
+        
