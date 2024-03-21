5e5c19a1c2d6b364a6163420c46cf9d61487d240
Arun Amballa <53054775+ArunAmballa@users.noreply.github.com>
2024-03-21 09:43:05 -0700

    Time: 61 ms (61.51%) | Memory: 18 MB (24.01%) - LeetSync

--- 120-triangle/triangle.py
+++ 120-triangle/triangle.py
@@ -1,19 +1,16 @@
 class Solution:
-    def helper(self,i,j,triangle,n,dp):
-        if i==n-1:
+    def helper(self,i,j,n,triangle,dp):
+        if i==n:
             return triangle[i][j]
         if dp[i][j]!=-1:
             return dp[i][j]
-        dp[i][j]=triangle[i][j]+min(self.helper(i+1,j,triangle,n,dp),self.helper(i+1,j+1,triangle,n,dp))
+        down=triangle[i][j]+self.helper(i+1,j,n,triangle,dp)
+        downRight=triangle[i][j]+self.helper(i+1,j+1,n,triangle,dp)
+        dp[i][j]=min(down,downRight)
         return dp[i][j]
     def minimumTotal(self, triangle: List[List[int]]) -> int:
         n=len(triangle)
-        prev=[0]*(n)
-        for j in range(0,n):
-            prev[j]=triangle[n-1][j]
-        for i in range(n-2,-1,-1):
-            curr=[0]*n
-            for j in range(i,-1,-1):
-                curr[j]=triangle[i][j]+min(prev[j],prev[j+1])
-            prev=curr
-        return prev[0]
+        m=len(triangle[n-1])
+        dp=[[-1 for j in range(m)]for i in range(n)]
+        return self.helper(0,0,n-1,triangle,dp)
+        
