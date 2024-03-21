1a5011792c3737afb16ea440d91a0938ccdda37c
Arun Amballa <53054775+ArunAmballa@users.noreply.github.com>
2024-03-20 19:55:33 -0700

    Time: 51 ms (14.93%) | Memory: 16.5 MB (90.77%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.py
+++ 63-unique-paths-ii/unique-paths-ii.py
@@ -1,20 +1,22 @@
 class Solution:
-    def helper(self,n,m,obstacleGrid,dp):
-        if obstacleGrid[n][m]==1:
-            return 0
-        if n==0 and m==0:
-            return 1
-        if n<0 or m<0:
-            return 0
-        if dp[n][m]!=-1:
-            return dp[n][m]
-        top=self.helper(n-1,m,obstacleGrid,dp)
-        left=self.helper(n,m-1,obstacleGrid,dp)
-        dp[n][m]=top+left
-        return dp[n][m]
     def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
         n=len(obstacleGrid)
         m=len(obstacleGrid[0])
-        dp=[[-1 for j in range(m)]for i in range(n)]
-        return self.helper(n-1,m-1,obstacleGrid,dp)
+        dp=[[0 for j in range(m)]for i in range(n)]
+        for i in range(0,n):
+            for j in range(0,m):
+                if obstacleGrid[i][j]==1:
+                    dp[i][j]=0
+                elif i==0 and j==0:
+                    dp[i][j]=1
+                else:
+                    top=0
+                    left=0
+                    if i>0:
+                        top=dp[i-1][j]
+                    if j>0:
+                        left=dp[i][j-1]
+                    dp[i][j]=top+left
+        return dp[n-1][m-1]
+
         
