94e357f06f55217880747e1b7a417fd3b0635b04
Arun Amballa <53054775+ArunAmballa@users.noreply.github.com>
2024-03-20 20:17:36 -0700

    Time: 86 ms (49.00%) | Memory: 18.3 MB (79.98%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.py
+++ 64-minimum-path-sum/minimum-path-sum.py
@@ -6,25 +6,13 @@ class Solution:
             return (1<<31)-1
         if dp[n][m]!=-1:
             return dp[n][m]
-        top=self.helper(n-1,m,grid,dp)
-        left=self.helper(n,m-1,grid,dp)
-        dp[n][m]=grid[n][m]+min(top,left)
+        top=grid[n][m]+self.helper(n-1,m,grid,dp)
+        left=grid[n][m]+self.helper(n,m-1,grid,dp)
+        dp[n][m]=min(top,left)
         return dp[n][m]
     def minPathSum(self, grid: List[List[int]]) -> int:
         n=len(grid)
         m=len(grid[0])
-        dp=[[0 for j in range(m)]for i in range(n)]
-        for i in range(0,n):
-            for j in range(0,m):
-                if i==0 and j==0:
-                    dp[i][j]=grid[i][j]
-                else:
-                    top=1<<31
-                    left=1<<31
-                    if i>0:
-                        top=dp[i-1][j]
-                    if j>0:
-                        left=dp[i][j-1]
-                    dp[i][j]=grid[i][j]+min(top,left)
-        return dp[n-1][m-1]
+        dp=[[-1 for j in range(m)]for i in range(n)]
+        return self.helper(n-1,m-1,grid,dp)
         
