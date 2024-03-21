9951207de4a8d954edd1ea8ecaa225e1173f134f
Arun Amballa <53054775+ArunAmballa@users.noreply.github.com>
2024-03-20 20:06:43 -0700

    Time: 81 ms (73.49%) | Memory: 18.4 MB (31.88%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.py
+++ 64-minimum-path-sum/minimum-path-sum.py
@@ -1,26 +1,18 @@
 class Solution:
     def helper(self,n,m,grid,dp):
-        if n<0 or m<0:
-            return 1<<31
         if n==0 and m==0:
             return grid[n][m]
+        if n<0 or m<0:
+            return (1<<31)-1
         if dp[n][m]!=-1:
             return dp[n][m]
-        dp[n][m]=grid[n][m]+min(self.helper(n-1,m,grid,dp),self.helper(n,m-1,grid,dp))
+        top=self.helper(n-1,m,grid,dp)
+        left=self.helper(n,m-1,grid,dp)
+        dp[n][m]=grid[n][m]+min(top,left)
         return dp[n][m]
     def minPathSum(self, grid: List[List[int]]) -> int:
         n=len(grid)
         m=len(grid[0])
-        prev=[0]*(m)
-        for i in range(0,n):
-            curr=[0]*(m)
-            for j in range(0,m):
-                if i==0 and j==0:
-                    curr[j]=grid[i][j]
-                    continue
-                up=prev[j] if i-1>=0 else 1<<31
-                left=curr[j-1] if j-1>=0 else 1<<31
-                curr[j]=grid[i][j]+min(up,left)
-            prev=curr
-        return curr[m-1]
+        dp=[[-1 for j in range(m)]for i in range(n)]
+        return self.helper(n-1,m-1,grid,dp)
         
