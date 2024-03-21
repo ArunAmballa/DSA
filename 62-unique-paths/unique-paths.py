4dd19fe8e1d6b675a13a75901f656ddd8e6f2ef0
Arun Amballa <53054775+ArunAmballa@users.noreply.github.com>
2024-03-20 19:41:55 -0700

    Time: 22 ms (99.49%) | Memory: 16.5 MB (87.21%) - LeetSync

--- 62-unique-paths/unique-paths.py
+++ 62-unique-paths/unique-paths.py
@@ -11,6 +11,19 @@ class Solution:
         dp[n][m]=top+left
         return dp[n][m]
     def uniquePaths(self, n: int, m: int) -> int:
-        dp=[[-1 for j in range(m)] for i in range(n)]
-        return self.helper(n-1,m-1,dp)
+        dp=[[0 for j in range(m)] for i in range(n)]
+        
+        for i in range(0,n):
+            for j in range(0,m):
+                if i==0 and j==0:
+                    dp[i][j]=1
+                else:
+                    top=0
+                    left=0
+                    if i>=0:
+                        top=dp[i-1][j]
+                    if j>=0:
+                        left=dp[i][j-1]
+                    dp[i][j]=top+left
+        return dp[n-1][m-1]
         
