5bc0a279aeff49b7a36a162bbdcd923b51cfb12f
Arun Amballa <53054775+ArunAmballa@users.noreply.github.com>
2024-03-20 18:47:40 -0700

    Time: 37 ms (48.20%) | Memory: 16.4 MB (87.21%) - LeetSync

--- 62-unique-paths/unique-paths.py
+++ 62-unique-paths/unique-paths.py
@@ -2,8 +2,6 @@ class Solution:
     def helper(self,n,m,dp):
         if n==0 or m==0:
             return 1
-        if n<0 or m<0:
-            return 0
         if dp[n][m]!=-1:
             return dp[n][m]
         top=self.helper(n-1,m,dp)
@@ -14,7 +12,12 @@ class Solution:
         dp=[[-1 for j in range(m)] for i in range(n)]
         # for i in range(0,n):
         #     for j in range(0,m):
-        #         if i==0 and 
+        #         if i==0 and j==0:
+        #             dp[i][j]=1
+        #             continue
+        #         top=dp[i-1][j]
+        #         left=dp[i][j-1]
+        #         dp[i][j]=top+left
 
         return self.helper(n-1,m-1,dp)
         
