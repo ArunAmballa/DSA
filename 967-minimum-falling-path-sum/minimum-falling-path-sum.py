a4f86de7ca358db26c0ba61ae6a750181d694d05
Arun Amballa <53054775+ArunAmballa@users.noreply.github.com>
2024-03-21 10:58:33 -0700

    Time: 140 ms (34.54%) | Memory: 17.3 MB (77.67%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.py
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.py
@@ -1,33 +1,38 @@
 class Solution:
-    def helper(self,i,j,matrix,n,dp):
-        if j<0 or j>=n:
+    def helper(self,n,m,matrix,c,dp):
+        if m<0 or m>=c:
             return 1<<31
-        if i==0:
-            return matrix[0][j]
-        if dp[i][j]!=-1:
-            return dp[i][j]
-        left=matrix[i][j]+self.helper(i-1,j-1,matrix,n,dp)
-        down=matrix[i][j]+self.helper(i-1,j,matrix,n,dp)
-        right=matrix[i][j]+self.helper(i-1,j+1,matrix,n,dp)
-        dp[i][j]=min(left,min(down,right))
-        return dp[i][j]
+        if n==0:
+            return matrix[n][m]
+        if dp[n][m]!=-1:
+            return dp[n][m]
+        top=matrix[n][m]+self.helper(n-1,m,matrix,c,dp)
+        topRight=matrix[n][m]+self.helper(n-1,m+1,matrix,c,dp)
+        topLeft=matrix[n][m]+self.helper(n-1,m-1,matrix,c,dp)
+        dp[n][m]=min(top,min(topLeft,topRight))
+        return dp[n][m]
     def minFallingPathSum(self, matrix: List[List[int]]) -> int:
         ans=1<<31
         n=len(matrix)
-        # dp=[[0 for j in range(n)]for i in range(n)]
-        prev=[0]*(n)
-        for j in range(n):
-            prev[j]=matrix[0][j]
-        for i in range(1,n):
-            curr=[0]*n
-            for j in range(0,n):
-                left=matrix[i][j]+(prev[j-1] if j-1>=0 else 1<<31)
-                down=matrix[i][j]+(prev[j])
-                right=matrix[i][j]+(prev[j+1] if j+1<n else 1<<31)
-                curr[j]=min(left,min(down,right))
-            prev=curr
-        for j in range(0,n):
-            ans=min(ans,prev[j])
+        m=len(matrix[0])
+        dp=[[-1 for j in range(m)]for i in range(n)]
+        for i in range(0,n):
+            for j in range(0,m):
+                if i==0:
+                    dp[i][j]=matrix[i][j]
+                else:
+                    top=matrix[i][j]+dp[i-1][j]
+                    if j+1<n:
+                        topRight=matrix[i][j]+dp[i-1][j+1]
+                    else:
+                        topRight=1<<31
+                    if j-1>=0:
+                        topLeft=matrix[i][j]+dp[i-1][j-1]
+                    else:
+                        topLeft=1<<31
+                    dp[i][j]=min(top,min(topLeft,topRight))
+        for j in range(0,m):
+            ans=min(ans,dp[n-1][j])            
         return ans
-            
+        
         
