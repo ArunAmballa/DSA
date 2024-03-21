275e586bc52b456b0835c4aaabe47c539e11d51f
Arun Amballa <53054775+ArunAmballa@users.noreply.github.com>
2024-03-21 09:50:29 -0700

    Time: 65 ms (37.90%) | Memory: 17.9 MB (24.01%) - LeetSync

--- 120-triangle/triangle.py
+++ 120-triangle/triangle.py
@@ -12,5 +12,16 @@ class Solution:
         n=len(triangle)
         m=len(triangle[n-1])
         dp=[[-1 for j in range(m)]for i in range(n)]
+        for i in range(n-1,-1,-1):
+            for j in range(i,-1,-1):
+                if i==n-1:
+                    dp[i][j]=triangle[i][j]
+                else:
+                    down=triangle[i][j]+dp[i+1][j]
+                    downRight=triangle[i][j]+dp[i+1][j+1]
+                    dp[i][j]=min(down,downRight)
+        return dp[0][0]
+
+
         return self.helper(0,0,n-1,triangle,dp)
         
