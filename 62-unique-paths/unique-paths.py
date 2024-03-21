45daca1fdc8af59c009ba2e9b1ade6af06f435d7
Arun Amballa <53054775+ArunAmballa@users.noreply.github.com>
2024-03-20 18:43:12 -0700

    Time: 43 ms (10.97%) | Memory: 16.5 MB (87.21%) - LeetSync

--- 62-unique-paths/unique-paths.py
+++ 62-unique-paths/unique-paths.py
@@ -1,29 +1,16 @@
 class Solution:
-    def helper(self,m,n,dp):
-        if m==0 and n==0:
+    def helper(self,n,m,dp):
+        if n==0 and m==0:
             return 1
-        if m<0 or n<0:
+        if n<0 or m<0:
             return 0
-        if dp[m][n]!=-1:
-            return dp[m][n]
-        dp[m][n]=self.helper(m-1,n,dp)+self.helper(m,n-1,dp)
-        return dp[m][n]
-    def uniquePaths(self, m: int, n: int) -> int:
-        
-        # return self.helper(m-1,n-1,dp)
-        prev=[0]*(n)
-        for i in range(m):
-            curr=[0]*(n)
-            for j in range(n):
-                if i<0 or j<0:
-                    curr[j]=0
-                    continue
-                if i==0 and j==0:
-                    curr[j]=1
-                    continue
-                curr[j]=prev[j]+curr[j-1]
-            prev=curr
-        return curr[n-1]
-
-
+        if dp[n][m]!=-1:
+            return dp[n][m]
+        top=self.helper(n-1,m,dp)
+        left=self.helper(n,m-1,dp)
+        dp[n][m]=top+left
+        return dp[n][m]
+    def uniquePaths(self, n: int, m: int) -> int:
+        dp=[[-1 for j in range(m)] for i in range(n)]
+        return self.helper(n-1,m-1,dp)
         
