83dd837d9d12fb30c26858eb9fe90d4c54d0757c
Arun Amballa <53054775+ArunAmballa@users.noreply.github.com>
2024-03-20 19:53:04 -0700

    Time: 43 ms (63.94%) | Memory: 16.6 MB (53.45%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.py
+++ 63-unique-paths-ii/unique-paths-ii.py
@@ -1,28 +1,20 @@
 class Solution:
-    # def helper(self,n,m,obstacleGrid):
-    #     if obstacleGrid[n][m]==1:
-    #         return 0
-    #     if n<0 or m<0:
-    #         return 0
-    #     if n==0 and m==0:
-    #         return 1
-    #     return self.helper(n-1,m,obstacleGrid)+self.helper(n,m-1,obstacleGrid)
+    def helper(self,n,m,obstacleGrid,dp):
+        if obstacleGrid[n][m]==1:
+            return 0
+        if n==0 and m==0:
+            return 1
+        if n<0 or m<0:
+            return 0
+        if dp[n][m]!=-1:
+            return dp[n][m]
+        top=self.helper(n-1,m,obstacleGrid,dp)
+        left=self.helper(n,m-1,obstacleGrid,dp)
+        dp[n][m]=top+left
+        return dp[n][m]
     def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
         n=len(obstacleGrid)
         m=len(obstacleGrid[0])
-        prev=[0]*(m)
-        for i in range(0,n):
-            curr=[0]*(m)
-            for j in range(0,m):
-                if obstacleGrid[i][j]==1:
-                    curr[j]=0
-                    continue
-                if i<0 or j<0:
-                    curr[j]=0
-                    continue
-                if i==0 and j==0:
-                    curr[j]=1
-                    continue
-                curr[j]=prev[j]+curr[j-1]
-            prev=curr
-        return curr[m-1]
+        dp=[[-1 for j in range(m)]for i in range(n)]
+        return self.helper(n-1,m-1,obstacleGrid,dp)
+        
