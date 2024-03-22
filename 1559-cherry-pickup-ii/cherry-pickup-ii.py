83b3a97c7126f620d04a2709079d0d39e3e0cc0c
Arun Amballa <53054775+ArunAmballa@users.noreply.github.com>
2024-03-22 11:10:18 -0700

    Time: 1096 ms (32.31%) | Memory: 22.5 MB (68.94%) - LeetSync

--- 1559-cherry-pickup-ii/cherry-pickup-ii.py
+++ 1559-cherry-pickup-ii/cherry-pickup-ii.py
@@ -1,24 +1,26 @@
 class Solution:
-    def maxPickup(self,i,j1,j2,grid,dp):
-            if j1 < 0 or j2 < 0 or j1 >= len(grid[0]) or j2 >= len(grid[0]): return -1e8
-            if (dp[i][j1][j2] != -1): return dp[i][j1][j2]
-            if (i == len(grid)-1):
-                if (j1 == j2): return grid[i][j1]
-                else: return grid[i][j1] + grid[i][j2]
-            maxi = -1e8
-            for dj1 in range(-1,2):
-                for dj2 in range(-1,2):
-                    value = 0
-                    if (j1 == j2):
-                        value = grid[i][j1]
-                    else:
-                        value = grid[i][j1] + grid[i][j2]
-                    value += self.maxPickup(i+1,j1+dj1,j2+dj2,grid,dp)
-                    maxi = max(maxi,value)
-            dp[i][j1][j2] = maxi
+    def helper(self,i,j1,j2,n,m,grid,dp):
+        if j1<0 or j2<0 or j1>=m or j2>=m:
+            return -int(1e9)
+        if i==n-1:
+            if j1==j2:
+                return grid[i][j1]
+            else:
+                return grid[i][j1]+grid[i][j2]
+        if dp[i][j1][j2]!=-1:
             return dp[i][j1][j2]
+        maxi=-int(1e9)
+        for dj1 in range(-1,2):
+            for dj2 in range(-1,2):
+                if j1==j2:
+                    maxi=max(maxi,grid[i][j1]+self.helper(i+1,j1+dj1,j2+dj2,n,m,grid,dp))
+                else:
+                    maxi=max(maxi,grid[i][j1]+grid[i][j2]+self.helper(i+1,j1+dj1,j2+dj2,n,m,grid,dp))
+        dp[i][j1][j2]=maxi
+        return dp[i][j1][j2]
     def cherryPickup(self, grid: List[List[int]]) -> int:
-        n = len(grid)
-        m = len(grid[0]) 
-        dp = [[[-1 for i in range(m+1)] for j in range(m+1)] for k in range(n+1)]
-        return self.maxPickup(0,0,m-1,grid,dp)
+        n=len(grid)
+        m=len(grid[0])
+        dp=[[[-1 for j in range(m)]for j in range(m)]for i in range(n)]
+        return self.helper(0,0,m-1,n,m,grid,dp)
+        
