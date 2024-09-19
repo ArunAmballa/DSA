a97089202c0d2cc0bd807a030b8ee652eb23cafd
Arun Amballa <53054775+ArunAmballa@users.noreply.github.com>
2024-09-18 22:25:32 -0700

    Time: 2 ms (97.62%) | Memory: 43.5 MB (63.76%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -3,22 +3,19 @@ class Solution {
         if(s.length()!=t.length()){
             return false;
         }
-        int []arr1=new int[26];
-        int []arr2=new int[26];
-        for(int i=0;i<s.length();i++){
-            char ch1=s.charAt(i);
-            char ch2=t.charAt(i);
-            int a=(int) ch1;
-            int b=(int) ch2;
-            arr1[a-97]=arr1[a-97]+1;
-            arr2[b-97]=arr2[b-97]+1;
+        int []count=new int[26];
+        for(char x:s.toCharArray()){
+            count[x-'a']++;
         }
-        for(int i=0;i<26;i++){
-            if(arr1[i]!=arr2[i]){
+
+        for(char x:t.toCharArray()){
+            count[x-'a']--;
+        }
+        for(int val:count){
+            if(val!=0){
                 return false;
             }
         }
-
         return true;
         
     }
