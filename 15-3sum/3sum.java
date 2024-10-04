f31934a69c6df6580a58eaaa554fa57abeb98b23
Arun Amballa <53054775+ArunAmballa@users.noreply.github.com>
2024-10-04 11:05:08 -0700

    Time: 33 ms (40.92%) | Memory: 52.4 MB (22.39%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,23 +1,23 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
+
         Arrays.sort(nums);
 
         int length=nums.length;
 
         List<List<Integer>> ans=new ArrayList<>();
 
-        for(int i=0;i<length;i++){
+        for(int i=0;i<length-2;i++){
 
             if(i>0 && nums[i]==nums[i-1]){
                 continue;
             }
-            
+
             int low=i+1;
             int high=length-1;
-
             while(low<high){
-                if(nums[low]+nums[high]+nums[i]==0){
-                    ArrayList<Integer> list=new ArrayList<>();
+                if(nums[i]+nums[low]+nums[high]==0){
+                    List<Integer> list=new ArrayList<>();
                     list.add(nums[i]);
                     list.add(nums[low]);
                     list.add(nums[high]);
@@ -27,18 +27,18 @@ class Solution {
                     while(low<high && nums[low]==nums[low-1]){
                         low=low+1;
                     }
-
-                    while(low<high && nums[high]==nums[high+1] ){
+                    while(low<high && nums[high]==nums[high+1]){
                         high=high-1;
                     }
-                    
-                }else if(nums[low]+nums[high]+nums[i]<0){
+                }else if((nums[i]+nums[low]+nums[high])<0){
                     low=low+1;
                 }else{
                     high=high-1;
                 }
             }
+
         }
+
 
         return ans;
         
