c4a82fd4be9efac7b56178512391f76df6369e2b
Arun Amballa <53054775+ArunAmballa@users.noreply.github.com>
2024-10-04 10:35:14 -0700

    Time: 9 ms (54.17%) | Memory: 45 MB (18.99%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -1,24 +1,27 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
 
-        int []ans=new int[2];
         int length=nums.length;
-        int [][]eleIndex=new int[length][2];
+
+        int [][]temp=new int[length][2];
+
+        int []ans=new int[2];
 
         for(int i=0;i<length;i++){
-            eleIndex[i][0]=nums[i];
-            eleIndex[i][1]=i;
+            temp[i][0]=nums[i];
+            temp[i][1]=i;
         }
-        Arrays.sort(eleIndex,(a,b)->Integer.compare(a[0],b[0]));
+
+        Arrays.sort(temp,(a,b)->Integer.compare(a[0],b[0]));
+
         int low=0;
         int high=length-1;
-
-        while(low<=high){
-            if(eleIndex[low][0]+eleIndex[high][0]==target){
-                ans[0]=eleIndex[low][1];
-                ans[1]=eleIndex[high][1];
-                return ans;
-            }else if(eleIndex[low][0]+eleIndex[high][0]<target){
+        while(low<high){
+            if(temp[low][0]+temp[high][0]==target){
+                ans[0]=temp[low][1];
+                ans[1]=temp[high][1];
+                break;
+            }else if(temp[low][0]+temp[high][0]<target){
                 low=low+1;
             }else{
                 high=high-1;
@@ -27,6 +30,5 @@ class Solution {
 
         return ans;
     }
+
 }
-
-
