b1c7502e62c1cde48158dbe70533d90c086e6c22
Arun Amballa <53054775+ArunAmballa@users.noreply.github.com>
2024-10-10 17:02:21 -0700

    Time: 0 ms (100.00%) | Memory: 44.7 MB (12.61%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.java
+++ 141-linked-list-cycle/linked-list-cycle.java
@@ -14,14 +14,15 @@ public class Solution {
         if(head==null || head.next==null){
                 return false;
         }
-        HashSet<ListNode> hs=new HashSet<>();
-        while(head!=null){
-            if(hs.contains(head)){
+        ListNode slow=head;
+        ListNode fast=head;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+            if(slow==fast){
                 return true;
             }
-            hs.add(head);
-            head=head.next;
         }
-        return false;  
+        return false;
     }
 }
