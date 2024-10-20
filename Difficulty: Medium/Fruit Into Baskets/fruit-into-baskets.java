727b5dd3b251639838d4259ac9cf1cea58528334
Arun Amballa <53054775+ArunAmballa@users.noreply.github.com>
2024-10-19 17:23:58 -0700

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Fruit Into Baskets/fruit-into-baskets.java
@@ -0,0 +1,67 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int t;
+        t = sc.nextInt();
+        sc.nextLine(); // Consume the newline character
+
+        while (t-- > 0) {
+            String line = sc.nextLine();
+            String[] tokens = line.split(" ");
+
+            // Create an ArrayList to store the integers
+            ArrayList<Integer> array = new ArrayList<>();
+
+            // Parse the tokens into integers and add to the array
+            for (String token : tokens) {
+                array.add(Integer.parseInt(token));
+            }
+
+            // Convert ArrayList to array
+            Integer[] arr = new Integer[array.size()];
+            array.toArray(arr);
+
+            Solution ob = new Solution();
+
+            // Call totalFruits method and print result
+            Integer ans = ob.totalFruits(arr);
+            System.out.println(ans);
+        }
+        sc.close();
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    public static int totalFruits(Integer[] arr) {
+        int length=arr.length;
+        int left=0;
+        int right=0;
+        int maxi=0;
+        HashMap<Integer,Integer> map=new HashMap<>();
+        while(right<length){
+            map.put(arr[right],map.getOrDefault(arr[right],0)+1);
+            while(map.size()>2){
+                map.put(arr[left],map.get(arr[left])-1);
+                if(map.get(arr[left])==0){
+                    map.remove(arr[left]);
+                }
+                left=left+1;
+            }
+            if(map.size()<=2){
+                maxi=Math.max(maxi,right-left+1);
+            }
+            right++;
+        }
+        return maxi;
+    }
+}
